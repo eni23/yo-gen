d06cec7d6c349e99ae2406d835fc0eb1c4b4b86a
Cyrill von Wattenwyl <eni@e23.ch>
2019-05-18 15:02:35 +0200

    add files

---
+++ jquery.figlet.js
@@ -0,0 +1,33 @@
+/**
+ * Figlet JS jQuery Plugin
+ *
+ * Copyright (c) 2010 Scott González
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * http://github.com/scottgonzalez/figlet-js
+ *
+ * Requires figlet.js
+ */
+(function($) {
+
+Figlet.loadFont = function(name, fn) {
+    var prefix = name.charAt(0) === '/' ? name : ("fonts/" + name)
+    $.ajax({
+	url: prefix + ".flf",
+	dataType: "text",
+	success: fn
+    });
+};
+
+$.fn.figlet = function(text, font) {
+	var elems = this;
+	Figlet.write(text, font || $.fn.figlet.defaultFont, function(str) {
+		elems.text(str);
+	});
+	return this;
+};
+
+$.fn.figlet.defaultFont = "banner";
+
+})(jQuery);
